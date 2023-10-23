aa4e5a53c5dcbdec8486f3949990fbe4e06bd444
Logan Prust <ljprust@uwm.edu>
2023-10-23 10:43:25 -0700

    added box for mesa eos test

---
+++ src/pgen/mesa_box.cpp
@@ -0,0 +1,66 @@
+//========================================================================================
+// Athena++ astrophysical MHD code
+// Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
+// Licensed under the 3-clause BSD License, see LICENSE file for details
+//========================================================================================
+//! \file shock_tube.cpp
+//! \brief Problem generator for shock tube problems.
+//!
+//! Problem generator for shock tube (1-D Riemann) problems. Initializes plane-parallel
+//! shock along x1 (in 1D, 2D, 3D), along x2 (in 2D, 3D), and along x3 (in 3D).
+//========================================================================================
+
+// C headers
+
+// C++ headers
+#include <cmath>      // sqrt()
+#include <cstdio>     // fopen(), freopen(), fprintf(), fclose()
+#include <iostream>   // endl
+#include <sstream>    // stringstream
+#include <stdexcept>  // runtime_error
+#include <string>
+
+// Athena++ headers
+#include "../athena.hpp"
+#include "../athena_arrays.hpp"
+#include "../coordinates/coordinates.hpp"
+#include "../eos/eos.hpp"
+#include "../field/field.hpp"
+#include "../hydro/hydro.hpp"
+#include "../mesh/mesh.hpp"
+#include "../parameter_input.hpp"
+#include "../scalars/scalars.hpp"
+
+namespace {
+Real rho0, vel0, pres0, gammagas;
+} // namespace
+
+void Mesh::InitUserMeshData(ParameterInput *pin) {
+  // Get parameters for gravitatonal potential of central point mass
+  rho0  = pin->GetOrAddReal("problem","rho0",1.0);
+  pres0 = pin->GetOrAddReal("problem","pres0",1.0);
+  vel0  = pin->GetOrAddReal("problem","vel0",0.0);
+  gammagas = pin->GetOrAddReal("hydro","gamma",0.0);
+  return;
+}
+
+//========================================================================================
+//! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
+//! \brief Problem Generator for the shock tube tests
+//========================================================================================
+
+void MeshBlock::ProblemGenerator(ParameterInput *pin) {
+
+  for (int k=ks; k<=ke; ++k) {
+    for (int j=js; j<=je; ++j) {
+      for (int i=is; i<=ie; ++i) {
+        phydro->u(IDN,k,j,i) = rho0;
+        phydro->u(IM1,k,j,i) = vel0;
+        phydro->u(IM2,k,j,i) = 0.0;
+        phydro->u(IM3,k,j,i) = 0.0;
+        phydro->u(IEN,k,j,i) = pres0/(gammagas-1.0) + 0.5*rho0*vel0*vel0;
+      }
+    }
+  }
+  return;
+}
